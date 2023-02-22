1905289845d1819829755b1845538f0db3ca93c0
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-02-21 22:20:22 -0300

    Implementando metodos do Repositorio de Serie

---
+++ Classes/Serie.cs
@@ -0,0 +1,53 @@
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        //Atributos
+
+        private Genero Genero { get; set; }
+
+        private string Titulo { get; set; }
+
+        private string Descricao { get; set; }
+
+        private int Ano { get; set; }
+
+        public bool Excluido { get; set; }
+
+        //Metodos
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Genero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descricao: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Inicio: " + this.Ano;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+    }
+}
---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,38 @@
+using DIO.Series.Interfaces;
+
+namespace DIO.Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
